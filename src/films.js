6196c8d26cb8590d05ce1cd11000a7ff40340a3a
Pablo <pablosdurbano@gmail.com>
2023-01-10 12:04:09 +0100

    exercice 1, 2 & 3

---
+++ src/films.js
@@ -0,0 +1,80 @@
+const movies = require("./data.js");
+
+// Exercise 1: Get the array of all directors.
+function getAllDirectors(array) {
+  const directors = array.map(movie => movie.director);
+  return directors;
+}
+
+// Exercise 2: Get the films of a certain director
+function getMoviesFromDirector(array, director) {
+ const moviesDirector = array.filter(movies => movies.director === director);
+ return moviesDirector;
+}
+ 
+// Exercise 3: Calculate the average of the films of a given director.
+function moviesAverageOfDirector(movies, director) {
+  
+    // 1. Filtra las películas por el director dado
+    const filmsByDirector = movies.filter(movie => movie.director === director);
+
+    // 2. si no hay peliculas del director
+    if(filmsByDirector.length === 0){
+      return 0
+  }
+  
+    // 3. Calcula la suma de las calificaciones de las películas
+    const sumOfRatings = filmsByDirector.reduce((acc, movie) => acc + movie.score, 0);  
+
+    // 4. Calcula el promedio de las calificaciones de las películas
+    const averageRating = (sumOfRatings / filmsByDirector.length);
+
+    // 5. Redondear el promedio a 2 decimales
+    const roundedRating = Math.round(averageRating * 100) / 100;
+    console.log(averageRating);
+
+    // 6. Devolver el promedio
+    return roundedRating;
+}
+
+// Exercise 4:  Alphabetic order by title 
+function orderAlphabetically(array) {
+  
+}
+
+// Exercise 5: Order by year, ascending
+function orderByYear() {
+
+}
+
+// Exercise 6: Calculate the average of the movies in a category
+function moviesAverageByCategory() {
+
+}
+
+// Exercise 7: Modify the duration of movies to minutes
+function hoursToMinutes() {
+
+}
+
+// Exercise 8: Get the best film of a year
+function bestFilmOfYear() {
+  
+}
+
+
+
+// The following is required to make unit tests work.
+/* Environment setup. Do not modify the below code. */
+if (typeof module !== 'undefined') {
+  module.exports = {
+    getAllDirectors,
+    getMoviesFromDirector,
+    moviesAverageOfDirector,
+    orderAlphabetically,
+    orderByYear,
+    moviesAverageByCategory,
+    hoursToMinutes,
+    bestFilmOfYear,
+  };
+}
